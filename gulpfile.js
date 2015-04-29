5cf90141e84afef1bad17cd3abd20c0e6c95be7e
Bohmaster <voidhmaster@gmail.com>
2015-04-29 07:05:03 -0300

    added include scripts task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,28 @@
 var gulp = require('gulp'),
     compass = require('gulp-compass');
+    inject = require('gulp-inject');
 
-gulp.task('test', function() {
-	return 'Task completed';
+gulp.task('default', function() {
+	console.log('Gulp running')
 });
+
+gulp.task('watch', ['index'], function() {
+	gulp.watch('./client/app/assets/scripts/*.js');
+});
+
+gulp.task('index', function() {
+	var target = gulp.src('./client/app/index.html');
+	var sources = gulp.src(
+		[
+			'./client/app/assets/scripts/*.js',
+		 	'./client/app/assets/styles/css/*.css'
+		],
+		{
+			read: false
+		}
+	);
+
+	return target.pipe(inject(sources))
+		.pipe(gulp.dest('./client/app'));
+});
+
